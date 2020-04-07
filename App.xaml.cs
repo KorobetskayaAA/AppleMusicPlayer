70b58171c4ae1b27e2e38c9ab9afadb0d161d496
KorobetskayaAA <kornast@yandex.ru>
2020-04-07 23:34:19 +0400

    Rss Feed

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace AppleMusicRssPlayer
+namespace AppleMusicPlayer
 {
     /// <summary>
     /// Interaction logic for App.xaml
